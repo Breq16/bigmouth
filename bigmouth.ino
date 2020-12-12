9deeb1265b2b00da36bb68fc7aacf29119afb795
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-12-12 17:42:08 -0500

    Add music player and sleep

--- bigmouth.ino
+++ bigmouth.ino
@@ -1,22 +1,38 @@
 #include "fish.h"
+#include <Adafruit_VS1053.h>
+#include <SD.h>
+#include <avr/sleep.h>
 
 Fish fish;
 
+Adafruit_VS1053_FilePlayer musicPlayer(-1, 7, 6, 3, 4);
+
 void setup() {
-  // put your setup code here, to run once:
   fish.begin();
+
+  musicPlayer.begin();
+  SD.begin(4);
+  musicPlayer.setVolume(20, 20);
+  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);
+  // musicPlayer.startPlayingFile("/track001.mp3"); // (full length version)
+  musicPlayer.startPlayingFile("/track002.mp3"); // (10 second version for testing)
+
+  while (!musicPlayer.stopped()) {
+    fish.head();
+    delay(1000);
+    fish.tail();
+    delay(1000);
+    fish.rest();
+    delay(1000);
+    fish.setMouth(1);
+    delay(1000);
+    fish.setMouth(0);
+    delay(1000);
+  }
+
+  sleep_enable();
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_cpu();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  fish.head();
-  delay(1000);
-  fish.tail();
-  delay(1000);
-  fish.rest();
-  delay(1000);
-  fish.setMouth(1);
-  delay(1000);
-  fish.setMouth(0);
-  delay(1000);
-}
+void loop() {}
